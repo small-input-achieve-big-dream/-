f3d1c6432f823b291194e94a8ebd58a1e8d36ba6
mymymy <1059117321@qq.com>
2019-06-12 12:50:23 +0800

    小马同志

--- insurance/settings.py
+++ insurance/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'insurance',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '1059117321',
         'HOST': 'localhost',
         'PORT': '3306',
     }
