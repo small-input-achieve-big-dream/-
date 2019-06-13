08b7783d604926ded873aecfd17ed208da7a214f
mymymy <1059117321@qq.com>
2019-06-13 20:23:20 +0800

    修改了admin后台和主页

--- message/admin.py
+++ message/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from message.models import *
- 
+
+@admin.register(accident_Application)
+class accident_Application_Admin(admin.ModelAdmin):
+    list_display =  ('applicationID' ,'tableID','state','compensation_money')
+
+
+
 # Register your models here.
-admin.site.register([compensate_Records,accident_Application,products,trade_Records,profit,table,user_login,applicant,recognizee_Infor,realtionship,complainInfor])
+admin.site.register([compensate_Records,products,trade_Records,profit,table,user_login,applicant,recognizee_Infor,realtionship,complainInfor])
+
