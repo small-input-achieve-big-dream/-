ede5f7503c7a647fc53b4dd9692556f2406d8357
jiangyifan123 <985897924@qq.com>
2019-06-13 20:24:59 +0800

    修改了admin

--- message/admin.py
+++ message/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from message.models import *
  
+@admin.register(accident_Application)
+class accident_Application_Admin(admin.ModelAdmin):
+	list_display = ('applicationID','tableID')
+ 		
 # Register your models here.
-admin.site.register([compensate_Records,accident_Application,products,trade_Records,profit,table,user_login,applicant,recognizee_Infor,realtionship,complainInfor])
+admin.site.register([compensate_Records,products,trade_Records,profit,table,user_login,applicant,recognizee_Infor,realtionship,complainInfor])
